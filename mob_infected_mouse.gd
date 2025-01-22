49ac70fb9c6961b9fa641e1534b388f933d1a469
maarken <ericpree@gmail.com>
2025-01-21 21:24:31 -0800

    Basic game
    character that moves, enemies that move, camera follows player, base sword sprite

---
+++ mob_infected_mouse.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+
+var mob_1_speed = 150.0
+@onready var player=get_node("/root/Gramrise/Player")
+
+func _physics_process(_delta):
+	var direction=global_position.direction_to(player.global_position)
+	velocity=direction*mob_1_speed
+	move_and_slide()
---
+++ player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+#var velocity = Vector2.ZERO
+var max_speed = 200.0
+var accel = 1500.0
+var friction = 600.0
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, accel * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide()
+
+	if velocity.length()>0.0:
+		%player_sprite.play("walk")
+	else:
+		%player_sprite.play("idle")
