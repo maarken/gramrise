e82ee3318011e7926b4447f1a83a73c92d110c1f
Silentknight84 <dylanbonsell@gmail.com>
2025-01-26 22:13:42 -0800

    Added external spawn, rotating sword, and on hit

--- mob_plague_bat.gd
+++ mob_plague_bat.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 var mob_1_speed = 80.0
 @onready var player=get_node("/root/Gramrise/Player")
 
+
 func _physics_process(_delta):
 	var direction=global_position.direction_to(player.global_position)
 	velocity=direction*mob_1_speed
--- player.gd
+++ player.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
+
+var score = 0
+var health = 10
+
 #var velocity = Vector2.ZERO
 var max_speed = 200.0
 var accel = 1500.0
 var friction = 600.0
 
+func add_score():
+	score += 1
+	print(score)
+	
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
